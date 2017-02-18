d6fa18a1c215c1f1a8b0ed8e7f8be95b75afe229
claudio.dobniewski <claudiod@gmail.comj>
2017-02-18 18:32:55 -0300

    * New helper class
    - clean trash code

---
+++ src/IntrawayBundle/Tools/ControllerHelper.php
@@ -0,0 +1,12 @@
+<?php
+namespace IntrawayBundle\Tools;
+
+class ControllerHelper{
+    
+    /**
+     * @return string - length 13 random hexa chars
+     */
+    public static function getRandomId(){
+        return uniqid(dechex(rand()));
+    }
+}
