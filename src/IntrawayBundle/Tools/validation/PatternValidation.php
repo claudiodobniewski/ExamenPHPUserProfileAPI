dc795d14dd4be7d9aefb5eb84e5accae6a297cc5
claudio.dobniewski <claudiod@gmail.comj>
2017-02-18 10:49:31 -0300

    * Check String common patterns helper

---
+++ src/IntrawayBundle/Tools/validation/PatternValidation.php
@@ -0,0 +1,34 @@
+<?php
+namespace IntrawayBundle\Tools\validation;
+
+class PatternValidation{
+    
+    /**
+     * Validate si un String corresponde en sintaxis a una URL
+     * Check if an Stirng var have lid URL sintax
+     * 
+     * @param String $url
+     * @return boolean
+     */
+    public static function valdateUrlPattern($url){
+        $validate = false;
+        if (!filter_var($url, FILTER_VALIDATE_URL) === false) {
+            $validate = true;
+        }
+        return $validate;
+    }
+    
+    /**
+     * Valida
+     * @param String $email
+     * @return boolean
+     */
+    public static function valdateEmailPattern($email){
+        $validate = false;
+        if (!filter_var($email, FILTER_VALIDATE_EMAIL) === false) {
+            $validate = true;
+        }
+        return $validate;
+    }
+    
+}
