ac116cebac84778ae8017fd478e3589ecf451a32
claudio.dobniewski <claudiod@gmail.comj>
2017-02-18 21:39:09 -0300

    PSR-2 COMPLIANCE - by php-cs-fix , and fix duplicate method

--- tests/IntrawayBundle/Controller/UserControllerTest.php
+++ tests/IntrawayBundle/Controller/UserControllerTest.php
@@ -13,11 +13,10 @@ class UserControllerTest extends WebTestCase
         $crawler = $client->request('GET', '/userProfile/1');
  //   {"id":1,"name":"Jorge Perez","email":"jorge.perez73@hotmail.com","Image":null}
         $expectedResponse = array('id' => 1,'name' => 'Jorge Perez','email' => 'jorge.perez73@hotmail.com','Image' => null);
-        $currentResponse = json_decode($client->getResponse()->getContent(),true);
+        $currentResponse = json_decode($client->getResponse()->getContent(), true);
         
         $this->assertEquals(200, $client->getResponse()->getStatusCode());
         $this->assertEquals($expectedResponse, $currentResponse);
-        
     }
     
     public function testGetUserNotExist()
@@ -29,24 +28,9 @@ class UserControllerTest extends WebTestCase
         //   {"message":"NOT FOUND USER [Id:548b50a158a8d785a9beb] [UserId:9999]"}
         
         $this->assertEquals(404, $client->getResponse()->getStatusCode());
-        $currentResponse = json_decode($client->getResponse()->getContent(),true);
+        $currentResponse = json_decode($client->getResponse()->getContent(), true);
         
-        $this->assertTrue( array_key_exists('message', $currentResponse));
-        $this->assertTrue(  strpos($currentResponse['message'],'NOT FOUND USER') !== false );
-    }
-    
-    public function testGetUserNotExist()
-    {
-        $client = static::createClient();
-    
-        $crawler = $client->request('GET', '/userProfile/999999999');
-    
-        //   {"message":"NOT FOUND USER [Id:548b50a158a8d785a9beb] [UserId:9999]"}
-    
-        $this->assertEquals(404, $client->getResponse()->getStatusCode());
-        $currentResponse = json_decode($client->getResponse()->getContent(),true);
-    
-        $this->assertTrue( array_key_exists('message', $currentResponse));
-        $this->assertTrue(  strpos($currentResponse['message'],'NOT FOUND USER') !== false );
+        $this->assertTrue(array_key_exists('message', $currentResponse));
+        $this->assertTrue(strpos($currentResponse['message'], 'NOT FOUND USER') !== false);
     }
 }
